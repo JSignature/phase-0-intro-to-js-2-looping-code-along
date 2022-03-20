3b2c7b31de7fc2b75f0c95a581274e0386f497fb
Jimmy Andreas <jimmysignature@gmail.com>
2022-03-19 22:30:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+function writeCards(name, event) {
+    const messages = []
+
+for (let index = 0; index < name.length; index++) {
+    
+    messages.push(`Thank you, ${name[index]}, for the wonderful ${event} gift!`)
+     
+}
+
+return messages
+    
+}
+
+
+
+
+function countDown(i) {
+
+    while (i >= 0) {
+
+        console.log(i)
+        i--
+    
+    }
+    /*for (let index = 10; index >= 0; index--) {
+        
+        console.log(index)
+    }*/
+    
+} 
+
+
+
